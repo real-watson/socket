24e2eba5d0b427694c04355566bbc54d89c75eb7
root <watson@example.com>
2020-02-02 15:53:35 +0800

    Fixed 	Added file lock.
    	modified:   server.c

--- server.c
+++ server.c
@@ -6,11 +6,38 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <fcntl.h>
 #define BUFF_SIZE_12K 1024*12
 #define PATH "video.mp4"
 unsigned int img_index = 0;
 
 pthread_mutex_t mutex;//pthread lock 
+
+static int init_lock(FILE *file, int type)
+{
+    struct flock lock;
+    int fd = 0;
+
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0;
+    //lock.l_type = type;
+    lock.l_pid = -1;
+
+    //to fd
+    fd = fileno(file);
+
+    //get lock status
+    fcntl(fd,F_GETLK,&lock);
+    if (lock.l_type == F_RDLCK || lock.l_type == F_WRLCK)
+    	return -1;
+    
+    //set lock
+    lock.l_type = type;
+    if ((fcntl(fd,F_SETLKW,&lock)) < 0)
+    	return -1;
+}
+
 static int recv_video_from_client(unsigned int connfd)
 {
     FILE *video = NULL;
@@ -20,7 +47,9 @@ static int recv_video_from_client(unsigned int connfd)
     video = fopen(PATH,"wb");
     if (NULL == video)
 	return -1;
-	
+    //lock file avoid others writting file
+    if (-1 == init_lock(video,F_WRLCK))
+    	return -1;
     //read video from server
     while((len = recv(connfd,buff,BUFF_SIZE_12K,0)) > 0)
         fwrite(buff,len,1,video);		
