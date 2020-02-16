6356589d5d2d382a5b1e848a7463ca2f3563eb6f
root <watson@example.com>
2020-02-16 17:11:55 +0800

    Fixed replace the size of ipaddr. 	modified:   include/common.h

--- include/common.h
+++ include/common.h
@@ -1,4 +1,5 @@
 #ifndef __INIT_H__
+
 #include <stdio.h>
 #include <pthread.h>
 #define BUFF_SIZE_12K 1024*12
@@ -6,41 +7,40 @@
 #define PATH "video.mp4"
 #define PATH_MESG "."
 #define VERSION "2.01"
-
+#define IP_ADDR_SIZE 64
 pthread_mutex_t mutex;//pthread lock 
 
 /*two c files include these variables*/
 static unsigned int img_index = 0;
 static unsigned int id_index = 200;
 
-typedef struct ip
+//single link
+typedef struct link
 {
-	char ipaddr[32];
+	char ipaddr[IP_ADDR_SIZE];
 	unsigned int port;
-	struct ip *next;
+	struct link *next;
 }LINKS;
 
+//double link
 typedef struct dlink
 {
 	unsigned int index;	
 	unsigned int port;
-	char ipaddr[32];
+	char ipaddr[IP_ADDR_SIZE];
 	struct dlink *lnext;
 	struct dlink *rnext;
 }DLINKS;
 
 extern int init_socket_server();
-
 extern void print_links(LINKS *head,DLINKS *dhead);
 extern void create_links(LINKS **head, LINKS *input);
-
 extern void double_create_links(DLINKS **dhead, DLINKS *dinput);
 extern void init_version();
 extern int recv_video_from_client(unsigned int connfd);
 extern int init_lock(FILE *file, int type);
 extern void *client_process(void *arg);
 extern int recv_image_from_client(int connfd);
+
 #define __INIT_H__
-
-
 #endif
