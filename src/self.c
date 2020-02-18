283232c600ad8a30e22367a42c024d3f9d6bff2c
root <watson@example.com>
2020-02-18 18:15:01 +0800

    Fixed added the last file. 	new file:   src/self.c

---
+++ src/self.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*save way for copying string from others' modification*/
+int mystrncpy(const char *src, char *dst, unsigned int n)
+{
+	if (!n)
+		return -1;
+	/*
+	*	--n ->strlen(src) + 1
+	*	n-- ->strlen(src)
+	*	check the last c is '\0' of src
+	*/
+	while(n--)
+	{
+		if ((*dst++ = *src++) == '\0')
+			break;
+	}
+	return 0;
+}
