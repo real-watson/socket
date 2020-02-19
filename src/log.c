c6773fa45b643950a2a3ab31a47982456eda5a0d
root <watson@example.com>
2020-02-19 17:50:28 +0800

    Fixed added memset for buff. 	modified:   log.c

--- src/log.c
+++ src/log.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define LOG_STORE(string,ret) (log_store_to_file(string,__FILE__,__func__,__LINE__))
+#include <string.h>
+//#include "common.h"
 
 int log_store_to_file(char *string, const char *filepath, const char *function, const int line)
 {
-	char tmplog[256] = "";
+	char tmplog[256];
 	char *ptr = NULL;
 	ptr = (char*)malloc(sizeof(char)*256);
 	ptr = tmplog;
-	if (ptr == NULL)
+	if (ptr == NULL || string == NULL)
 		return -1;
 	else
 		sprintf(ptr,"echo string:[%s] filepath:[%s] function:[%s] line:[%d] >> ../logfile/store.log",string,filepath,function,line);
 	if (-1 == system(ptr))
 		return -1;
+
+	memset(tmplog,0,256);
+	memset(string,0,256);
+
 	return 0;
 }
 
