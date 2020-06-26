af8fe74266e691b9cc07f1ea96315ddbf26ea0f6
real-watson <watsonwaitingme@gmail.com>
2020-06-26 08:21:45 +0800

    增加arm版库方式
    	modified:   build.sh

--- src/socketlib/build.sh
+++ src/socketlib/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#add arm arm-linux-gnueabi-gcc
+#arm-linux-gnueabi-gcc -fPIC -c socket.c
+#arm-linux-gnueabi-gcc -fPIC -shared -o libsocket.so socket.o
 
 gcc -fPIC -c socket.c
 gcc -fPIC -shared -o libsocket.so socket.o
